68077b8c433015848e5c2f1c8061ba49e5d0be17
victoriamonteiro <victoria.vrsm@gmail.com>
2026-09-11 13:23:59 -0300

    padrão de inicializacao do NEW

---
+++ src/Arrays02.java
@@ -0,0 +1,24 @@
+public class Arrays02 {
+    public static void main(String[] args) {
+       // byte, short, int, long, float e double é [0]
+        int[] idades = new int[3];
+        // O que tem lá? [0, 0, 0]
+
+        double[] salarios = new double[3];
+        // O que tem lá? [0.0, 0.0, 0.0]
+
+        boolean[] status = new boolean[3];
+        // O que tem lá? [false, false, false]
+
+        String[] nomes = new String[3];
+        // O que tem lá? [null, null, null]
+
+       /* idades[0] = 20;
+        idades[1] = 27;
+        idades[2] = 34; */
+
+        System.out.println(status [0]);
+        System.out.println(status [1]);
+        System.out.println(status [2]);
+    }
+}
